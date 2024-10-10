ce74f03ce2f5a6a1c6b1ee37372941dfdc9e3c7f
Mike Earley <mike@5sn1.com>
2024-10-10 09:29:44 -0400

    ran cleaner; removed all things supabase; added mononoki font

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,9 +38,6 @@ export default defineConfig({
     schema: {
       PUBLIC_ASTRO_CLIENT_API_URL: envField.string({ context: 'client', access: 'public' }),
       PUBLIC_ASTRO_SERVER_API_URL: envField.string({ context: 'server', access: 'public' }),
-      PUBLIC_SUPABASE_URL: envField.string({ context: 'client', access: 'public' }),
-      PUBLIC_SUPABASE_ANON_KEY: envField.string({ context: 'client', access: 'public' }),
-      SUPABASE_SERVICE_ROLE_KEY: envField.string({ context: 'server', access: 'secret' }),
     },
   },
   output: 'server',
