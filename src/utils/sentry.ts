665f1eb959aabeb2d1f9ca65bcb3e010be1e39de
Mike Earley <mike@5sn1.com>
2024-10-28 15:41:04 -0400

    updates

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -5,8 +5,14 @@ export const initSentry = () => {
   Sentry.init({
     dsn: import.meta.env.SENTRY_DSN,
     environment: import.meta.env.PROD ? 'production' : 'development',
-
+    release: 'learnit@' + process.env.npm_package_version,
+    enableTracing: true,
     tracesSampleRate: 1.0,
     debug: !import.meta.env.PROD,
+    integrations: [
+      Sentry.captureConsoleIntegration(),
+      Sentry.httpClientIntegration(),
+      Sentry.sessionTimingIntegration(),
+    ],
   })
 }
