6f579159dd3836462280e23fb37380b38e7e32e5
Mike Earley <mike@5sn1.com>
2024-10-28 13:22:33 -0400

    trying to make package versioning work correctly; added scripts for it; sentry.ts change is nothing

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -5,6 +5,7 @@ export const initSentry = () => {
   Sentry.init({
     dsn: import.meta.env.SENTRY_DSN,
     environment: import.meta.env.PROD ? 'production' : 'development',
+
     tracesSampleRate: 1.0,
     debug: !import.meta.env.PROD,
   })
